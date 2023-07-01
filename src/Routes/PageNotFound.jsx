d46b8e53396bab0bdc7dbf5eaac59d74bcfd4cb8
NoeZur <noeliazurbriggen@hotmail.com>
2023-06-30 23:37:33 -0300

    ultimo final de verdad commit

---
+++ src/Routes/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useContextGlobal } from '../utils/Context'
+
+const PageNotFound = () => {
+  const { state, dispatch}= useContextGlobal();
+  return (
+    <div className={state.theme == 'light' ? "light" : "dark"}>
+        <h1>Esta página existe en otro multiverso, pero en este No! 😊 Error 404</h1>
+    </div>
+  )
+}
+
+export default PageNotFound
